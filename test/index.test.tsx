df7a7cb0acd4d78eb37016d718bd62a9b3c30806
Gustavo <gustavo.shigueo@proton.me>
2023-09-06 14:52:39 -0300

    Remove tests for demo code

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,9 +1,7 @@
-import { isServer } from 'solid-js/web'
 import { describe, expect, it } from 'vitest'
 
 describe('environment', () => {
   it('runs on server', () => {
-    expect(typeof window).toBe('object')
-    expect(isServer).toBe(false)
+    expect(true).toBe(true)
   })
 })
